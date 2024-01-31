6e50d5665558e256050a03a8df13e4d77892f60a
Brandman303 <124716614+Brandman303@users.noreply.github.com>
2024-01-31 10:30:07 -0700

    created parkingKiosk program

---
+++ parkingKiosk/parkingKiosk/parkingKiosk.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int totalCost(int hours, int vehicleCost);
+
+int main() {
+    string yesNo;
+    int hours;
+    int vehicleType;
+    int vehicleCost[] = { 2, 3, 4 };
+    const string VEHICLE[] = { "car", "bus", "truck" };
+
+    cout << "Do you want to park? y/n" << endl;
+    cin >> yesNo;
+    transform(yesNo.begin(), yesNo.end(), yesNo.begin(), ::tolower);
+
+    if (yesNo == "y" || yesNo == "yes") {
+        bool trueFalse = true;
+        while (trueFalse) {
+            cout << "Please enter the number of hours you wish to park in integer form." << endl;
+            cin >> hours;
+
+            if (hours > 0) {
+                cout << "Please enter the number associated with your vehicle type:" << endl;
+                cout << "1. car" << endl;
+                cout << "2. bus" << endl;
+                cout << "3. truck" << endl;
+                cin >> vehicleType;
+
+                if (vehicleType >= 1 && vehicleType <= 3) {
+                    int finalTotal = totalCost(hours, vehicleCost[vehicleType - 1]);
+                    cout << "Total cost: $" << finalTotal << endl;
+                    trueFalse = false;
+                }
+                else {
+                    cout << "Incorrect input for vehicle type." << endl;
+                }
+            }
+            else {
+                cout << "Not a vaild input for number of hours" << endl;
+            }
+        
+        }
+    }
+    else {
+        cout << "Goodbye" << endl;
+    }
+
+    return 0;
+}
+
+int totalCost(int hours, int vehicleCost) {
+    return hours * vehicleCost;
+}
