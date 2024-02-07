5739a1c63d46bd4ee723da8df17748da85624176
Brandman303 <124716614+Brandman303@users.noreply.github.com>
2024-02-07 16:25:36 -0700

    fixed functions

--- creditCardValidation/creditCardValidation/creditCardValidation.cpp
+++ creditCardValidation/creditCardValidation/creditCardValidation.cpp
@@ -6,68 +6,29 @@ using namespace std;
 */
 
 // return true if the argument is a valid card number
-bool isValid(unsigned long long int) {
+bool isValid(unsigned long long int number);
 
-}
 
 // get the result from step 2
-int sumOfDoubleEvenPlace(unsigned long long int number) {
-	while ((number % 2) == 0 && number > 0) {
-		
-		int sumEven = 0;
-		sumEven += getDigit(number);
-		number /= 10;
+int sumOfDoubleEvenPlace(unsigned long long int number);
 
-	}
-	return sumEven;
-}
 
 /* return this number if argument is a single digit, otherwise
  * return the sum of the two digit argument
  */
-int getDigit(int) { // ask if this function is meant only for the sum right to left of every second number. or for both even and odd numbers
-	
-	int cardNum = int;
-	int lastDigit = cardNum % 10
+int getDigit(int num);
 
-	if ((lastDigit * 2) >= 10) { //this only needs to be executed for every second number from the right (on card num), not the first num. 
-		lastDigit *= 2;
-		return (lastDigit / 10) + (lastDigit % 10)
-	}
-	else {
-		return lastDigit;
-	}
-	return 0;
-}
-	
 
 // return sum of odd placed digits in argument
-int sumOfOddPlace(unsigned long long int number) {
-	while ((number % 2) != 0 % %number > 0) {
-		
-		int digit = number % 10;
-		int sumOdd += digit;
-		digit /= 10
+int sumOfOddPlace(unsigned long long int number);
 
-	}
-	return sumOdd;
-}
 
 // return true if prefix d is a prefix for number
 bool prefixMatched(unsigned long long int number, int d);
 
 // return the number of digits in argument
-int getSize(unsigned long long int number) {
-	int size = 0;
+int getSize(unsigned long long int number);
 
-	while (number > 0) { //make sure this doesn't count zero after going right to left. could have to do with pre and post iteration
-		
-		number /= 10;
-		size++;
-
-	}
-	return size;
-}
 
 /* return the first k number of digits from number.  If the
  * number of digits in number is is less than k, return number.
@@ -79,6 +40,61 @@ int main() {
 	cout << ((isValid(ccNum)) ? "True" : "False");
 }
 
-bool isValid(unsigned long long int num) {
-	return false;
+bool isValid(unsigned long long int number) {
+	int size;
+	int theSums;
+	int sumEven = sumOfDoubleEvenPlace(number); // corrected
+	int sumOdd = sumOfOddPlace(number); //need2fix
+
+
+	size = getSize(number);
+	theSums = sumEven + sumOdd;
+	
+	return (size >= 13 && size <= 16 && (theSums % 10) == 0) ? true : false;
+}
+int sumOfDoubleEvenPlace(unsigned long long int number) {
+	int sumEven = 0;
+	int lastDigit;
+	bool evenDigit = false;
+
+	while (number > 0) {
+		lastDigit = number % 10;
+		if (evenDigit) {
+			int doubledDigit = lastDigit * 2;
+			sumEven += getDigit(doubledDigit);
+		}
+		evenDigit = !evenDigit;
+		number /= 10;
+	}
+	return sumEven;
+}
+int getDigit(int num) { // ask if this function is meant only for the sum right to left of every second number. or for both even and odd number
+	return (num >= 10) ? ((num / 10) + (num % 10)) : num;
+}
+int sumOfOddPlace(unsigned long long int number) {
+	int lastDigit;
+	int sumOdd = 0;
+	bool oddDigit = true;
+	while (number > 0) {
+		lastDigit = number % 10;
+		if (oddDigit) {
+			sumOdd += lastDigit;
+		}
+		oddDigit = !oddDigit;
+		number /= 10;
+
+	}
+	return sumOdd;
+}
+int getSize(unsigned long long int number) {
+	int size;
+	size = 0;
+
+	while (number > 0) { //make sure this doesn't count zero after going right to left. could have to do with pre and post iteration
+
+		number /= 10;
+		size++;
+
+	}
+	return size;
 }
