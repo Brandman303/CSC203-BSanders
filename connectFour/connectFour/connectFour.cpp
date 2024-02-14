22e97974ab8ef396785199cd25c048ded4a57d3c
Brandman303 <124716614+Brandman303@users.noreply.github.com>
2024-02-13 21:37:48 -0700

    gameOverCheck needs fixing

--- connectFour/connectFour/connectFour.cpp
+++ connectFour/connectFour/connectFour.cpp
@@ -2,19 +2,28 @@
 using namespace std;
 
 void displayGrid();//formatted string to display a connect four grid
-bool gameOver(); //checks if there is a winner
+bool gameOverCheck(int grid[][7], int numDisk); //checks if there is a winner
 void createGridRow(); //this is used by displayGrid to produce a row
-void dropDisk(); // this lets player drop a disk in chosen column
+void dropDisk(int grid[][7], int numDisk, int colChoice); // this lets player drop a disk in chosen column
 void startingGridArray(int grid[][7], int const rows, int const cols);
 void printGridArray(int grid[][7], int const rows, int const cols);// to traverse the array and print the grid to see if game is working correctly
 int main() {
-	int numDisks = 42;
+	int numDisk = 42;
 	int const rows = 6;
 	int const cols = 7;
 	int grid[rows][cols];
+	int colChoice;
+	int gameOver = false;
 	startingGridArray(grid, rows, cols);
 	printGridArray(grid, rows, cols);
-	
+	do {
+		cout << "enter the column number you would like to drop your disk into: " << endl;
+		cin >> colChoice;
+		dropDisk(grid, numDisk, colChoice);
+		printGridArray(grid, rows, cols);
+		gameOver = gameOverCheck(grid, numDisk); //could probably squeeze this into the conditional statement and eliminate gameOver variable
+	} while (gameOver != true);
+	cout << "Game Over";
 	
 
 }
@@ -23,8 +32,10 @@ void startingGridArray(int grid[][7], int const rows, int const cols) {
 	for (int i = 0; i < rows; i++) { //builds the initial grid 0 are empty 1 is a disk
 		for (int j = 0; j < cols; j++) {
 			grid[i][j] = 0;
+
 		}
 	}
+	//grid[1][0] = 1; this is a test to see where 1 will be
 }
 void printGridArray(int grid[][7], int const rows, int const cols) {
 
@@ -33,5 +44,33 @@ void printGridArray(int grid[][7], int const rows, int const cols) {
 			cout << grid[i][j];
 		}
 		cout << endl;
+	}
+}
+
+void dropDisk(int grid[][7], int numDisk, int col) { //maybe pass by reference for numDisk
+	numDisk--; // I think this will need to be pass by reference or something else to directly change num disk
+	for (int i = 5; i >= 0; i--) {
+		if (grid[i][col] < 1) {
+			grid[i][col] = 1;
+			break;
+		}
+	}
+}
+
+bool gameOverCheck(int grid[][7], int numDisk) {
+	int counter = 0;
+	if (numDisk <= 0) {
+		return true;
+	}
+	for (int i = 0; i < 6; i++) { //traverses grid and checks for four in a row 
+		for (int j = 0; j < 7; j++) {
+			if (grid[i][j] != 0) {
+				counter++; //this is just if there is any 4 disks in the array which isnt right
+				if (counter == 4) {//maybe a switch statement would be more appropriate for determining the row and check?
+					return true;
+				}
+			}
+		}
+
 	}
 }
